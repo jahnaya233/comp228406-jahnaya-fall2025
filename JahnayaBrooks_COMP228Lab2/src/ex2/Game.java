36c1cb4f7a267a73acf141839a3b9c4109a8af08
Jahnaya <jbrook42@my.centennialcollege.ca>
2025-10-10 18:48:10 -0400

    Lab 2 work changes

--- JahnayaBrooks_COMP228Lab2/src/ex2/Game.java
+++ JahnayaBrooks_COMP228Lab2/src/ex2/Game.java
@@ -4,7 +4,7 @@ import javax.swing.JOptionPane;
 
 public class Game {
     public static void main(String[] args) {
-        String input = JOptionPane.showInputDialog("Enter something between 3 and 27");
+        String input = JOptionPane.showInputDialog("Enter a number between 3 and 27");
 
 
         int inputNumber = Integer.parseInt(input);
@@ -16,18 +16,17 @@ public class Game {
             int sum = lotto.getSum();
             int[] nums = lotto.getNumbers();
 
-            JOptionPane.showMessageDialog(null,"Roll " + attempt + ": " + nums[0] + ", " + nums[1] + ", " + nums[2] + "Sum: " + sum);
+            JOptionPane.showMessageDialog(null, "Roll " + attempt + ": " + nums[0] + ", " + nums[1] + ", " + nums[2] + "Sum: " + sum);
             if (sum == inputNumber) {
-                JOptionPane.showMessageDialog(null,"You have won!");
+                JOptionPane.showMessageDialog(null, "You have won!");
                 win = true;
                 break;
             }
-            
-    else{
-                    JOptionPane.showMessageDialog(null,
-                            "You have lost");
+        }
+            if(!win) {
+                JOptionPane.showMessageDialog(null, "Computer wins!");
                 }
             }
 
         }
-    }
+
--- JahnayaBrooks_COMP228Lab2/src/ex3/Exercise3Driver.java
+++ JahnayaBrooks_COMP228Lab2/src/ex3/Exercise3Driver.java
@@ -4,14 +4,14 @@ import static ex3.OverloadedMethods.sum;
 
 public class Exercise3Driver {
     public static void main(String[] args) {
-        int result = sum(5, 10);
-        System.out. println("The sum of 5 and 10 is: " + result);
+        int result = sum(3, 6);
+        System.out.println("The sum of 3 and 6 is: " + result);
 
-        int results = sum(3,6,9);
-        System.out. println("The sum of 3, 6, and 9 is: " + results);
+        int resultSec = sum(7,2,4);
+        System.out.println("The sum of 7, 2, and 4 is: " + resultSec);
 
-        int resultss = sum(1,4,7);
-        System.out. println("The sum of 1, 4, and 7 is: " + resultss);
+        int resultThird = sum(1,4,7);
+        System.out.println("The sum of 1, 4, and 7 is: " + resultThird);
 
     }
 }
